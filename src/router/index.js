58eb71575bfab7b733ef628a7d1cc18da5ff92a9
Taysumov Hadis <hadis_01@mail.ru>
2021-01-26 16:12:22 +0300

    создание и добавление всех шаблонов

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
   {
     path: '/login',
     name: 'login',
@@ -21,6 +15,42 @@ const routes = [
     name: 'categories',
     meta: { layout: 'main' },
     component: () => import('@/views/Categories.vue'),
+  },
+  {
+    path: '/detail',
+    name: 'detail',
+    meta: { layout: 'main' },
+    component: () => import('@/views/Detail.vue'),
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: { layout: 'main' },
+    component: () => import('@/views/History.vue'),
+  },
+  {
+    path: '/home',
+    name: 'home',
+    meta: { layout: 'main' },
+    component: () => import('@/views/Home.vue'),
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: { layout: 'main' },
+    component: () => import('@/views/Profile.vue'),
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: { layout: 'main' },
+    component: () => import('@/views/Record.vue'),
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: { layout: 'main' },
+    component: () => import('@/views/Planning.vue'),
   },
 ];
 
