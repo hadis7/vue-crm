48b86df4a51dbea642f7cd9684d11da6f1f301d4
Taysumov Hadis <hadis_01@mail.ru>
2021-02-01 21:16:37 +0300

    Добавлен Firebase

--- src/main.js
+++ src/main.js
@@ -9,14 +9,36 @@ import messagePlugin from '@/utils/message.plugin.js'
 import '@/registerServiceWorker.js';
 import 'materialize-css/dist/js/materialize.min';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
 Vue.config.productionTip = false;
 
 Vue.use(messagePlugin);
 Vue.use(Vuelidate);
 Vue.filter('date',dateFilter);
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+firebase.initializeApp({
+  apiKey: "AIzaSyC7PGbvwXd7wXz7RlvfPs3UerFoZ96Oplg",
+  authDomain: "vue-crm-77b5f.firebaseapp.com",
+  projectId: "vue-crm-77b5f",
+  storageBucket: "vue-crm-77b5f.appspot.com",
+  messagingSenderId: "794335329063",
+  appId: "1:794335329063:web:6d1ea53ff4574055450702",
+  measurementId: "G-K400V6FLHL"
+})
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+ if (!app) {
+  app = new Vue({
+    router,
+    store,
+    render: (h) => h(App),
+  }).$mount('#app');
+  }
+})
+
+
